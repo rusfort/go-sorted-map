3fa5bff32705c9b64e94856d7a12e40fb22b166c
rusfort <rusfort-fax@yandex.ru>
2023-05-08 12:29:53 +0300

    add reorder

--- map/base.go
+++ map/base.go
@@ -1,8 +1,9 @@
 package functions
 
 import (
-	//"context"
-	//"fmt"
+	"fmt"
+	"sort"
+	"sync"
 )
 
 type pair struct {
@@ -11,11 +12,48 @@ type pair struct {
 }
 
 type BaseMap struct {
+	mtx sync.Mutex //TODO: use!
+	size int
 	orderedKeys []interface{}
 	container []pair
+	m map[interface{}]interface{}
 }
 
 func NewMap() *BaseMap {
-	return &BaseMap{}
+	return &BaseMap{
+		m: make(map[interface{}]interface{}),
+	}
+}
+
+func (b *BaseMap) Map() map[interface{}]interface{} {
+	return b.m
+}
+
+func (b *BaseMap) reorder() {
+	if len(b.m) != b.size {
+		//TODO: refresh orderedKeys and container!
+	}
+	if len(b.orderedKeys) == 0 {
+		return
+	}
+	sort.Slice(b.orderedKeys, func(i, j int) bool {
+		switch b.orderedKeys[0].(type) {
+		case int:
+			return b.orderedKeys[i].(int) < b.orderedKeys[j].(int)
+		case int64:
+			return b.orderedKeys[i].(int64) < b.orderedKeys[j].(int64)
+		case float64:
+			return b.orderedKeys[i].(float64) < b.orderedKeys[j].(float64)
+		case string:
+			return b.orderedKeys[i].(string) < b.orderedKeys[j].(string)
+		default:
+			panic(fmt.Sprintf("type %T is not allowed as a map key", b.orderedKeys[0]))
+		}
+	})
+}
+
+func (b *BaseMap) Keys() []interface{} {
+	b.reorder()
+	return b.orderedKeys
 }
 
